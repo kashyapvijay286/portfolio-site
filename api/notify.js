7acb5d2b1a8e50a74fc56e52aff40a35203e0b38
VIJAY KUMAR KASHYAP <110352361+kashyapvijay286@users.noreply.github.com>
2026-06-02 11:56:01 +0530

    notify.js

--- api/notify.js
+++ api/notify.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
         headings: { en: title },
         contents: { en: message },
         // 2. Yahan fix link ki jagah hum dynamic 'url' use karenge
-        url: url || "https://theeha.vercel.app" 
+        url: url || "https://portfolio-site-indol-two-58.vercel.app/kalamkaari.html" 
     };
 
     try {
