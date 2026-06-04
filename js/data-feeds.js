41956cba3ce4f10767a90475b5f2d8699adc8dc9
VIJAY KUMAR KASHYAP <110352361+kashyapvijay286@users.noreply.github.com>
2026-06-04 16:48:28 +0530

    data-feeds.js

--- js/data-feeds.js
+++ js/data-feeds.js
@@ -252,7 +252,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             utterance.lang = 'hi-IN'; 
             utterance.rate = 0.7;     // Dheemi aawaz, natural flow ke liye optimal
-            utterance.pitch = 0.1;   // Hafiz Sahab ki bhaari aawaz ka magic
+            utterance.pitch = 1;   // Hafiz Sahab ki bhaari aawaz ka magic
 
             utterance.onend = function() {
                 btnElement.innerHTML = '🎙️';
